9deceb0bf92f62fbc766c70d85c9bc8f2eddecde
Lorentz Lasson <lasson.lorentz@gmail.com>
2017-03-02 00:39:07 +0100

    Seed with sample data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,28 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Glossmos.Repo.insert!(%Glossmos.SomeModel{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Glossmos.Repo
+alias Glossmos.Gloss
+alias Glossmos.GlossSet
+
+Repo.delete_all Gloss
+
+# ~w(gloss gloss_set gloss_set_bulk)
+# |> Enum.map(fn(f) ->
+#   File.read!("sample_data/#{f}.json")
+#   |> Poison.decode!
+# end)
+# |> List.flatten
+# |> IO.inspect
+
+g = File.read!("sample_data/gloss.json")
+|> Poison.decode!
+Gloss.changeset(%Gloss{}, g)
+|> Repo.insert
+
+File.read!("sample_data/gloss_set.json")
+|> Poison.decode!
+|> GlossSet.changesets
+|> GlossSet.insert
+
+File.read!("sample_data/gloss_set_bulk.json")
+|> Poison.decode!
+|> GlossSet.changesets_bulk
+|> GlossSet.insert_bulk
